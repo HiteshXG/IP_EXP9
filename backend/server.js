140f3dc73ed97231fced8fc42f85cd81e51495e1
HiteshXG <140910237+HiteshXG@users.noreply.github.com>
2024-10-06 13:10:12 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const PORT = 3001;
 const REVIEWS_FILE = path.join(__dirname, "reviews.json");
 
 const server = http.createServer(async (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
